87f4da626603314efcae6c65e9ed9878e3a10ea6
corycalaway <cory.c.calaway@gmail.com>
2020-12-12 14:33:26 -0700

    added some dates to 5 day and created layout

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,6 +1,13 @@
+var setTimeDate = function () {
+    
+    dateTime = luxon.DateTime.local();
+    
+    
+    currentWeatherFunction(dateTime)
+  };
 
 // create current weather from search
-var currentWeatherFunction = function() {
+var currentWeatherFunction = function(dateTime) {
 
 var currentWeather = document.querySelector('#currentWeather');
 
@@ -57,35 +64,53 @@ fiveDayForcastDays.classList.add("row");
 fiveDayForcastDays.classList.add("justify-content-between");
 fiveDayForcast.appendChild(fiveDayForcastDays);
 
+
 // create day one of five day forcast
-var fiveDayForcastDayOne = document.createElement('div');
-fiveDayForcastDayOne.classList.add("col-2");
-fiveDayForcastDayOne.textContent = "test";
+var fiveDayForcastDayOne = document.createElement('ul');
+fiveDayForcastDayOne.classList.add("col");
 fiveDayForcastDays.appendChild(fiveDayForcastDayOne);
+
+// list created for day one 
+var dayOneDate = document.createElement('li');
+dayOneDate.classList.add("list-group-item");
+fiveDayForcastDayOne.appendChild(dayOneDate);
+
+// add date time plus 1 day to forcast
+dayOneDate.textContent = dateTime.plus({ days: 1}).toLocaleString();
+
 
 // create day two of five day forcast
 var fiveDayForcastDayTwo = document.createElement('div');
-fiveDayForcastDayTwo.classList.add("col-2");
-fiveDayForcastDayTwo.textContent = "test2";
+fiveDayForcastDayTwo.classList.add("col");
 fiveDayForcastDays.appendChild(fiveDayForcastDayTwo);
+
+// list created for day two 
+var dayTwoDate = document.createElement('li');
+dayTwoDate.classList.add("list-group-item");
+fiveDayForcastDayTwo.appendChild(dayTwoDate);
+
+// add date time plus 2 day to forcast
+dayTwoDate.textContent = dateTime.plus({ days: 2}).toLocaleString();
+
 
 // create day three of five day forcast
 var fiveDayForcastDayThree = document.createElement('div');
-fiveDayForcastDayThree.classList.add("col-2");
+fiveDayForcastDayThree.classList.add("col");
 fiveDayForcastDayThree.textContent = "test3";
 fiveDayForcastDays.appendChild(fiveDayForcastDayThree);
 
 // create day four of five day forcast
 var fiveDayForcastDayFour = document.createElement('div');
-fiveDayForcastDayFour.classList.add("col-2");
+fiveDayForcastDayFour.classList.add("col");
 fiveDayForcastDayFour.textContent = "test4";
 fiveDayForcastDays.appendChild(fiveDayForcastDayFour);
 
 // create day five of five day forcast
 var fiveDayForcastDayFive = document.createElement('div');
-fiveDayForcastDayFive.classList.add("col-2");
+fiveDayForcastDayFive.classList.add("col");
 fiveDayForcastDayFive.textContent = "test5";
 fiveDayForcastDays.appendChild(fiveDayForcastDayFive);
 };
 
-currentWeatherFunction();
+//currentWeatherFunction();
+setTimeDate();
